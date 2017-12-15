522e038095832d61eff46ddb815051eb16b2043e
victor <victor@vbonhomme.fr>
2017-12-16 00:55:56 +0100

    add cache method

--- src/iexec-server-js-client.js
+++ src/iexec-server-js-client.js
@@ -7,9 +7,9 @@ const fetch = require('node-fetch');
 const qs = require('qs');
 const hash = require('hash.js');
 const request = require('request-promise');
-const { getApplicationBinaryFieldName, waitFor } = require('./utils');
+const { getAppBinaryFieldName, waitFor } = require('./utils');
 
-const debug = Debug('xwhep-js-client');
+const debug = Debug('iexec-server-js-client');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const createIEXECClient = ({
@@ -24,13 +24,13 @@ const createIEXECClient = ({
   const BASE_URL = 'https://'.concat(hostname, ':', port, '/');
   const STATE_AUTH = {};
   let mandatedLogin = mandated;
+  const APPS = {};
 
-  const http = method => async (endpoint, { uid = '', params = {}, body = {} }) => {
+  const http = method => async (endpoint, { uid = '', params = {}, body = {} } = {}) => {
     try {
       const MANDATED = mandatedLogin !== '' ? { XWMANDATINGLOGIN: mandatedLogin } : {};
       const allParams = Object.assign({}, params, STATE_AUTH, MANDATED);
       const queryString = Object.keys(allParams).length !== 0 ? '?'.concat(qs.stringify(allParams)) : '';
-
       const uri = BASE_URL.concat(endpoint, uid ? '/' : '', uid, queryString);
       const headers = { Authorization: 'Basic '.concat(BASICAUTH_CREDENTIALS) };
 
@@ -76,7 +76,8 @@ const createIEXECClient = ({
 
   const getUID = uid => get('get', { uid });
   const removeUID = uid => get('remove', { uid });
-  const getApps = () => get('getapps');
+  const getAppsUIDs = () => get('getapps').then(uids => uids.xwhep.XMLVector[0].XMLVALUE.map(e => e.$.value));
+  const getAppsByUIDs = appsUIDs => Promise.all(appsUIDs.map(uid => getUID(uid)));
   const getWorkByExternalID = uid => get('getworkbyexternalid', { uid });
   const sendData = xmlData => get('senddata', { params: { XMLDESC: xmlData } });
   const sendApp = xmlApp => get('sendapp', { params: { XMLDESC: xmlApp } });
@@ -93,9 +94,9 @@ const createIEXECClient = ({
     return post('uploaddata', { uid, body: form });
   };
 
-  const createWork = (uid, appuid, sgid) => `<work><uid>${uid}</uid><accessrights>0x755</accessrights><appuid>${appuid}</appuid><sgid>${sgid}</sgid><status>UNAVAILABLE</status></work>`;
-  const createData = (uid, type, cpu, os) => `<data><uid>${uid}</uid><accessrights>0x755</accessrights><type>${type}</type><name>fileName</name><cpu>${cpu}</cpu><os>${os}</os><status>UNAVAILABLE</status></data>`;
   const createApp = (uid, name, extraFields = {}) => `<app><uid>${uid}</uid><name>${name}</name><type>DEPLOYABLE</type><accessrights>0x755</accessrights>${json2xml(extraFields)}</app>`;
+  const createData = (uid, type, cpu, os) => `<data><uid>${uid}</uid><accessrights>0x755</accessrights><type>${type}</type><name>fileName</name><cpu>${cpu}</cpu><os>${os}</os><status>UNAVAILABLE</status></data>`;
+  const createWork = (uid, appuid, sgid) => `<work><uid>${uid}</uid><accessrights>0x755</accessrights><appuid>${appuid}</appuid><sgid>${sgid}</sgid><status>UNAVAILABLE</status></work>`;
 
   const registerApp = async (data, type, cpu, os, size, name) => {
     const dataUID = uuidV4();
@@ -103,30 +104,53 @@ const createIEXECClient = ({
     await sendData(createData(dataUID, type, cpu, os));
     await uploadData(dataUID, data, size);
     const fields = {};
-    fields[getApplicationBinaryFieldName(os, cpu)] = 'xw://xwserver/'.concat(dataUID);
+    fields[getAppBinaryFieldName(os, cpu)] = 'xw://xwserver/'.concat(dataUID);
     const appUID = uuidV4();
     debug('appUID', appUID);
     await sendApp(createApp(appUID, name, fields));
     return appUID;
   };
 
-  const submitWork = async (appuid, sgid) => {
+  const submitWork = async (appUID, sgid) => {
     const workUID = uuidV4();
     debug('workUID', workUID);
-    await sendWork(createWork(workUID, appuid, sgid));
-    await sendWork(createWork(workUID, appuid, sgid));
+    await sendWork(createWork(workUID, appUID, sgid));
+    await sendWork(createWork(workUID, appUID, sgid));
     const work = await getUID(workUID);
-    work.xwhep.work[0].status = 'PENDING';
+    debug('work.xwhep.work[0].status[0]', work.xwhep.work[0].status[0]);
+    work.xwhep.work[0].status[0] = 'PENDING';
     await sendWork(json2xml(work));
     return workUID;
   };
 
-  const waitForWorkCompleted = async uid => waitFor(getUID, uid);
+  const waitForWorkCompleted = async workUID => waitFor(getUID, workUID);
+
+  const appsToCache = apps => apps.forEach((app) => {
+    const appUID = app.xwhep.app[0].uid[0];
+    APPS[app.xwhep.app[0].name[0]] = appUID;
+  });
+
+  const updateAppsCache = async () => {
+    const appsUIDs = await getAppsUIDs();
+    const cacheAppsUIDs = Object.keys(APPS).map(name => APPS[name]);
+    debug('cacheAppsUIDs', cacheAppsUIDs);
+    const notCachedUIDs = appsUIDs.filter(uid => !cacheAppsUIDs.includes(uid));
+    debug('notCachedUIDs', notCachedUIDs);
+    const notCachedApps = await getAppsByUIDs(notCachedUIDs);
+    appsToCache(notCachedApps);
+  };
+
+  const submitWorkByAppName = async (appName, sgid) => {
+    if (!(appName in APPS)) await updateAppsCache();
+    if (!(appName in APPS)) throw Error(`No match for App name ${appName}`);
+    const appUID = APPS[appName];
+    debug('appUID', appUID, 'from name', appName);
+    return submitWork(appUID, sgid);
+  };
 
   const init = async () => {
     if (jwt) await getCookieByJWT(jwt);
-    const apps = await getApps();
-    debug('apps', apps);
+    await updateAppsCache();
   };
 
   return {
@@ -137,7 +161,10 @@ const createIEXECClient = ({
     setMandated,
     getUID,
     removeUID,
-    getApps,
+    getAppsUIDs,
+    getAppsByUIDs,
+    appsToCache,
+    updateAppsCache,
     getWorkByExternalID,
     sendWork,
     sendData,
@@ -147,6 +174,7 @@ const createIEXECClient = ({
     createWork,
     registerApp,
     submitWork,
+    submitWorkByAppName,
     waitForWorkCompleted,
   };
 };
