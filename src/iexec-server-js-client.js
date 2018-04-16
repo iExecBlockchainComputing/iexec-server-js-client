566090faf38c5879f33bf1aed0bace7fc63d8f4d
victor <victor@vbonhomme.fr>
2018-04-16 11:53:09 +0200

    fix style + add auth() method

--- src/iexec-server-js-client.js
+++ src/iexec-server-js-client.js
@@ -13,6 +13,7 @@ const { getAppBinaryFieldName, waitFor } = require('./utils');
 
 const debug = Debug('iexec-server-js-client');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const AUTH_URL = 'https://auth.iex.ec';
 
 const createIEXECClient = ({
   login = '',
@@ -20,10 +21,13 @@ const createIEXECClient = ({
   server = '',
   jwt = '',
   mandated = '',
+  cookie = '',
 }) => {
-  const BASICAUTH_CREDENTIALS = login ? Buffer.from(login.concat(':', password)).toString('base64') : undefined;
+  const BASICAUTH_CREDENTIALS = login
+    ? Buffer.from(login.concat(':', password)).toString('base64')
+    : undefined;
   debug('BASICAUTH_CREDENTIALS', BASICAUTH_CREDENTIALS);
-  const STATE_AUTH = {};
+  const STATE_AUTH = cookie ? { state: cookie } : {};
   let mandatedLogin = mandated;
   const APPS = {};
   const hostname = server.split('://')[1].split(':')[0];
@@ -46,15 +50,21 @@ const createIEXECClient = ({
     text: textFormat,
   };
 
-  const http = method => async (endpoint, {
-    uid = '', params = {}, body = undefined, format = xmlFormat,
-  } = {}) => {
+  const http = method => async (
+    endpoint,
+    {
+      uid = '', params = {}, body = undefined, format = xmlFormat,
+    } = {},
+  ) => {
     try {
       const MANDATED = mandatedLogin !== '' ? { XWMANDATINGLOGIN: mandatedLogin } : {};
       const allParams = Object.assign({}, params, STATE_AUTH, MANDATED);
-      const queryString = Object.keys(allParams).length !== 0 ? '?'.concat(qs.stringify(allParams)) : '';
+      const queryString =
+        Object.keys(allParams).length !== 0 ? '?'.concat(qs.stringify(allParams)) : '';
       const uri = server.concat('/', endpoint, uid ? '/' : '', uid, queryString);
-      const headers = BASICAUTH_CREDENTIALS ? { Authorization: 'Basic '.concat(BASICAUTH_CREDENTIALS) } : {};
+      const headers = BASICAUTH_CREDENTIALS
+        ? { Authorization: 'Basic '.concat(BASICAUTH_CREDENTIALS) }
+        : {};
 
       debug(method, uri);
       const res = await fetch(uri, {
@@ -74,9 +84,12 @@ const createIEXECClient = ({
 
   const getCookieByJWT = async (jwtoken) => {
     try {
-      const cookie = await get('ethauth/', { params: { ethauthtoken: jwtoken, noredirect: 'true' }, format: streamFormat }).then(res => res.text());
-      debug('cookie', cookie);
-      STATE_AUTH.state = cookie;
+      const authCookie = await get('ethauth/', {
+        params: { ethauthtoken: jwtoken, noredirect: 'true' },
+        format: streamFormat,
+      }).then(res => res.text());
+      debug('authCookie', authCookie);
+      STATE_AUTH.state = authCookie;
       return cookie;
     } catch (error) {
       debug('getCookieByJWT()', error);
@@ -84,12 +97,23 @@ const createIEXECClient = ({
     }
   };
 
-  function setMandated(mandatedUser) { mandatedLogin = mandatedUser; }
+  function setMandated(mandatedUser) {
+    mandatedLogin = mandatedUser;
+  }
 
-  const getUID = uid => get('get', { uid });
-  const removeUID = uid => get('remove', { uid });
+  const getByUID = uid => get('get', { uid });
+  const getUID = (uid) => {
+    console.log('deprecated, use getByUID() instead');
+    return getByUID(uid);
+  };
+  const removeByUID = uid => get('remove', { uid });
+  const removeUID = (uid) => {
+    console.log('deprecated, use removeByUID() instead');
+    return removeByUID(uid);
+  };
   const getAppByName = uid => get('getappbyname', { uid });
-  const getAppsUIDs = () => get('getapps').then(uids => uids.xwhep.XMLVector[0].XMLVALUE.map(e => e.$.value));
+  const getAppsUIDs = () =>
+    get('getapps').then(uids => uids.xwhep.XMLVector[0].XMLVALUE.map(e => e.$.value));
   const getAppsByUIDs = appsUIDs => Promise.all(appsUIDs.map(uid => getUID(uid)));
   const getWorkByExternalID = uid => get('getworkbyexternalid', { uid });
   const sendData = xmlData => get('senddata', { params: { XMLDESC: xmlData } });
@@ -99,13 +123,18 @@ const createIEXECClient = ({
   const uploadData = (uid, data, size) => {
     const form = new FormData();
     form.append('DATAUID', uid);
-    const sha256 = hash.sha256().update(data).digest('hex');
+    const sha256 = hash
+      .sha256()
+      .update(data)
+      .digest('hex');
     debug('sha256', sha256);
     form.append('DATAMD5SUM', sha256);
     form.append('DATASIZE', size);
     form.append('DATAFILE', data);
     return post('uploaddata', { uid, body: form });
   };
+  const createDownloadURI = workResultURI =>
+    server.concat(`/downloaddata/${uri2uid(workResultURI)}?state=${STATE_AUTH.state}`);
 
   const defaultApp = { accessrights: '0x1700', type: 'DEPLOYABLE' };
   const defaultData = { accessrights: '0x1700', name: 'fileName', status: 'UNAVAILABLE' };
@@ -143,10 +172,11 @@ const createIEXECClient = ({
 
   const waitForWorkCompleted = async workUID => waitFor(getUID, workUID);
 
-  const appsToCache = apps => apps.forEach((app) => {
-    const appUID = app.xwhep.app[0].uid[0];
-    APPS[app.xwhep.app[0].name[0]] = appUID;
-  });
+  const appsToCache = apps =>
+    apps.forEach((app) => {
+      const appUID = app.xwhep.app[0].uid[0];
+      APPS[app.xwhep.app[0].name[0]] = appUID;
+    });
 
   const updateAppsCache = async () => {
     const appsUIDs = await getAppsUIDs();
@@ -165,71 +195,109 @@ const createIEXECClient = ({
     return submitWork(appUID, params);
   };
 
-  const downloadStream = (uid, stream = '') => new Promise(async (resolve, reject) => {
-    const res = await get('downloaddata', { uid, format: streamFormat });
+  const downloadStream = (uid, stream = '') =>
+    new Promise(async (resolve, reject) => {
+      const res = await get('downloaddata', { uid, format: streamFormat });
 
-    let buff = Buffer.from('', 'utf8');
-    let full = false;
-    const bufferSize = 1 * 1024;
-    const outputStream = stream === '' ? devnull() : stream;
+      let buff = Buffer.from('', 'utf8');
+      let full = false;
+      const bufferSize = 1 * 1024;
+      const outputStream = stream === '' ? devnull() : stream;
 
-    res.body
-      .pipe(through2((chunk, enc, cb) => {
-        if (!full) {
-          buff = Buffer.concat([buff, chunk]);
-          if (buff.length >= bufferSize) {
-            debug('Buffer limit reached', buff.length);
-            full = true;
+      res.body
+        .pipe(through2((chunk, enc, cb) => {
+          if (!full) {
+            buff = Buffer.concat([buff, chunk]);
+            if (buff.length >= bufferSize) {
+              debug('Buffer limit reached', buff.length);
+              full = true;
+            }
           }
-        }
-        cb(null, chunk);
-      }))
-      .on('error', reject)
-      .pipe(outputStream)
-      .on('error', reject)
-      .on('finish', () => {
-        debug('finish event');
-        debug('buff.length', buff.length);
-        debug('buff.slice(0, bufferSize).length', buff.slice(0, bufferSize).length);
-        resolve({ stdout: buff.slice(0, bufferSize).toString() });
-      });
-  });
+          cb(null, chunk);
+        }))
+        .on('error', reject)
+        .pipe(outputStream)
+        .on('error', reject)
+        .on('finish', () => {
+          debug('finish event');
+          debug('buff.length', buff.length);
+          debug('buff.slice(0, bufferSize).length', buff.slice(0, bufferSize).length);
+          resolve({ stdout: buff.slice(0, bufferSize).toString() });
+        });
+    });
 
   const init = async () => {
     if (jwt) await getCookieByJWT(jwt);
     await updateAppsCache();
   };
 
-  return Object.assign({
-    server,
-    state: STATE_AUTH,
-    init,
-    get,
-    post,
-    getCookieByJWT,
-    setMandated,
-    getUID,
-    removeUID,
-    getAppByName,
-    getAppsUIDs,
-    getAppsByUIDs,
-    appsToCache,
-    updateAppsCache,
-    getWorkByExternalID,
-    sendWork,
-    sendData,
-    sendApp,
-    download,
-    downloadStream,
-    uploadData,
-    registerData,
-    registerApp,
-    submitWork,
-    submitWorkByAppName,
-    waitForWorkCompleted,
-    uri2uid,
-    uid2uri,
-    getAppBinaryFieldName,
-  }, utils);
+  const auth = async (ethProvider, address) => {
+    const { message } = await fetch(`${AUTH_URL}/typedmessage`).then(res => res.json());
+    debug('message', message);
+    const msgJSON = JSON.stringify(message);
+
+    // MetaMask signing
+    return new Promise((resolve, reject) => {
+      ethProvider.sendAsync(
+        {
+          method: 'eth_signTypedData',
+          params: [message, address],
+          from: address,
+        },
+        async (err, signature) => {
+          if (err) return reject(err);
+          debug('signature', signature);
+
+          const { jwtoken } = await fetch(`${AUTH_URL}/typedauth?message=${msgJSON}&address=${address}&signature=${
+            signature.result
+          }`).then(res => res.json());
+          debug('jwtoken', jwtoken);
+          const authCookie = await getCookieByJWT(jwtoken);
+          debug('authCookie', authCookie);
+          const version = get('version');
+          debug('version', version);
+
+          return resolve({ cookie: authCookie, jwtoken });
+        },
+      );
+    });
+  };
+
+  return Object.assign(
+    {
+      server,
+      state: STATE_AUTH,
+      init,
+      get,
+      post,
+      getCookieByJWT,
+      auth,
+      setMandated,
+      getUID,
+      removeUID,
+      getAppByName,
+      getAppsUIDs,
+      getAppsByUIDs,
+      appsToCache,
+      updateAppsCache,
+      getWorkByExternalID,
+      sendWork,
+      sendData,
+      sendApp,
+      download,
+      downloadStream,
+      uploadData,
+      createDownloadURI,
+      registerData,
+      registerApp,
+      submitWork,
+      submitWorkByAppName,
+      waitForWorkCompleted,
+      uri2uid,
+      uid2uri,
+      getAppBinaryFieldName,
+    },
+    utils,
+  );
 };
 module.exports = createIEXECClient;
